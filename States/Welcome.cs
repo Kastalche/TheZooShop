a7226282673eff02cfeff954f4dce0a6fa7b0860
Siyana <roronoazorri@gmail.com>
2022-11-08 17:00:47 +0200

    Update Welcome.cs

--- States/Welcome.cs
+++ States/Welcome.cs
@@ -15,12 +15,25 @@ namespace TheZooShop.States
 
         public void Start()
         {
+
             throw new NotImplementedException();
         }
         public void Destroy()
         {
             throw new NotImplementedException();
         }
+
+        OpenStore();
+        GreetCustomer();
+        TakeCustomerName();
+        ShowAnimals();
+        ChooseAnimal();
+        InsertMoney(); //-> NotEnoughMoney(); - throw special exception here.
+        BuyAnimal();
+        PlaywithAnimal();
+        FeedAnimal();
+        ExitShop()/EnterShopAgain();
+        //
 
     }
 }
